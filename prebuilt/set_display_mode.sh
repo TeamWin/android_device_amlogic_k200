36cdc7fa223479d8b041cfd54dc7b2cb4537d8b5
AbduL <goncalo77@vodafone.pt>
2015-03-28 23:06:43 +0000

    Update to 5.1 branch
    Change-Id: I74e86de41e64bef72902b944fe8344ae5eda3545
    
    Update graphics.c
    
    Change-Id: Ideeead0ec43df31d61cf061ccad43a11d3871e3c
    
    Clean display mode script
    
    Change-Id: I2fe183d8c383c666a3eb6861b01d83a67bf89581

--- prebuilt/set_display_mode.sh
+++ prebuilt/set_display_mode.sh
@@ -8,181 +8,136 @@ hpdstate=$(cat /sys/class/amhdmitx/amhdmitx0/hpd_state)
 old_state=$hpdstate
 
 if [ "$hpdstate" = "1" ]; then
-#zgq remove begin for: conflix in git merge s82
-#<<<<<<< HEAD
-#	if [ "$outputmode" = "480cvbs" -o "$outputmode" = "576cvbs" ] ; then
-#    outputmode=720p
-#  fi
-#else
-#	if [ "$outputmode" != "480cvbs" -a "$outputmode" != "576cvbs" ] ; then
-#=======
-#zgq remove end
-    while read line
-    do
-       if [ "$outputmode" = "$line" ]; then
-           support_flag=1
-           break;
-       else
-          support_flag=0
-      fi
-                    
+    while read line; do
+        if [ "$outputmode" = "$line" ]; then
+            support_flag=1
+            break;
+        else
+            support_flag=0
+        fi
         echo "$line" | busybox grep -q '*'
-        if [ $? -eq 0 ]
-        then
+        if [ $? -eq 0 ]; then
             bestmode=${line/'*'}
         fi
-    done < /sys/class/amhdmitx/amhdmitx0/disp_cap 
+    done < /sys/class/amhdmitx/amhdmitx0/disp_cap
 
     if [ "$support_flag" = "0" -a "$bestmode" != "0" ]; then
         outputmode=$bestmode
-    else 
+    else
         if [ "$support_flag" = "0" ]; then
             outputmode=$bestmode
         fi
     fi
-
 else
-        if [ "$outputmode" != "480cvbs" -a "$outputmode" != "576cvbs" ] ; then
-#>>>>>>> s82 #zgq remove
-    outputmode=576cvbs
-  fi
+    if [ "$outputmode" != "480cvbs" -a "$outputmode" != "576cvbs" ] ; then
+        outputmode=576cvbs
+    fi
 fi
-
 echo $outputmode > /sys/class/display/mode
-
 busybox echo 0 > /sys/class/ppmgr/ppscaler
 busybox echo 0 > /sys/class/graphics/fb0/free_scale
 busybox echo 1 > /sys/class/graphics/fb0/freescale_mode
 
+case $outputmode in
+    480*)
+    busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
+    busybox echo 30 20 689 459 > /sys/class/graphics/fb0/window_axis
+    ;;
 
-	case $outputmode in 
+    576*)
+    busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
+    busybox echo 30 20 689 555 > /sys/class/graphics/fb0/window_axis
+    ;;
 
-		480*)
-		busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
-		busybox echo 30 20 689 459 > /sys/class/graphics/fb0/window_axis 
-		;;
-    
-		576*)
-		busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
-		busybox echo 30 20 689 555 > /sys/class/graphics/fb0/window_axis 
-		;; 
+    720*)
+    busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
+    busybox echo 40 30 1239 689 > /sys/class/graphics/fb0/window_axis
+    ;;
 
-		720*)
-		busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
-		busybox echo 40 30 1239 689 > /sys/class/graphics/fb0/window_axis 
-		;; 
+    1080*)
+    busybox echo 0 0 1919 1079 > /sys/class/graphics/fb0/free_scale_axis
+    busybox echo 60 40 1859 1039 > /sys/class/graphics/fb0/window_axis
+    ;;
 
-		1080*)
-		busybox echo 0 0 1919 1079 > /sys/class/graphics/fb0/free_scale_axis
-		busybox echo 60 40 1859 1039 > /sys/class/graphics/fb0/window_axis 
-		;; 
+    4k2k*)
+    busybox echo 0 0 1919 1079 > /sys/class/graphics/fb0/free_scale_axis
+    busybox echo 120 80 3719 2079 > /sys/class/graphics/fb0/window_axis
+    ;;
 
-		4k2k*)
-		busybox echo 0 0 1919 1079 > /sys/class/graphics/fb0/free_scale_axis
-		busybox echo 120 80 3719 2079 > /sys/class/graphics/fb0/window_axis 
-		;;
- 
-		*)
-		#outputmode= 720p
-		echo 720p > /sys/class/display/mode  
-		busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
-		busybox echo 40 30 1239 689 > /sys/class/graphics/fb0/window_axis 
-
+    *)
+    #Default to 720p
+    echo 720p > /sys/class/display/mode
+    busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
+    busybox echo 40 30 1239 689 > /sys/class/graphics/fb0/window_axis
 esac
 
 busybox echo 0x10001 > /sys/class/graphics/fb0/free_scale
 busybox echo 1 > /sys/class/graphics/fb1/blank
 
-
-#for checking is hdmi or cvbs 
+#for checking is hdmi or cvbs
 while true ; do
-hpdstate=$(cat /sys/class/amhdmitx/amhdmitx0/hpd_state)
-if [ $hpdstate != $old_state ] ; then
-	old_state=$hpdstate
-	if [ "$hpdstate" = "1" ]; then
-#zgq remove begin for:conflix in git merge s82
-#<<<<<<< HEAD
-#		if [ "$outputmode" = "480cvbs" -o "$outputmode" = "576cvbs" ] ; then
-#			outputmode=720p
-#		fi
-#=======
-#zgq remove end
-			while read line
-      do
-          if [ "$outputmode" = "$line" ]; then
-              support_flag=1
-              break;
-          else
-              support_flag=0
-          fi
-                    
-          echo "$line" | busybox grep -q '*'
-          if [ $? -eq 0 ]
-          then
-              bestmode=${line/'*'}
-          fi
-    done < /sys/class/amhdmitx/amhdmitx0/disp_cap 
-
-    if [ "$support_flag" = "0" -a "$bestmode" != "0" ]; then
-        outputmode=$bestmode
-    else 
-        if [ "$support_flag" = "0" ]; then
-            outputmode=$bestmode
+    hpdstate=$(cat /sys/class/amhdmitx/amhdmitx0/hpd_state)
+    if [ $hpdstate != $old_state ] ; then
+        old_state=$hpdstate
+        if [ "$hpdstate" = "1" ]; then
+            while read line; do
+                if [ "$outputmode" = "$line" ]; then
+                    support_flag=1
+                    break;
+                else
+                    support_flag=0
+                fi
+                echo "$line" | busybox grep -q '*'
+                if [ $? -eq 0 ]; then
+                    bestmode=${line/'*'}
+                fi
+            done < /sys/class/amhdmitx/amhdmitx0/disp_cap
+            if [ "$support_flag" = "0" -a "$bestmode" != "0" ]; then
+                outputmode=$bestmode
+            else
+                if [ "$support_flag" = "0" ]; then
+                    outputmode=$bestmode
+                fi
+            fi
+        else
+            if [ "$outputmode" != "480cvbs" -a "$outputmode" != "576cvbs" ] ; then
+                outputmode=576cvbs
+            fi
         fi
+        echo $outputmode > /sys/class/display/mode
+        busybox echo 0 > /sys/class/ppmgr/ppscaler
+        busybox echo 0 > /sys/class/graphics/fb0/free_scale
+        busybox echo 1 > /sys/class/graphics/fb0/freescale_mode
+
+        case $outputmode in
+            480*)
+            busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
+            busybox echo 30 20 689 459 > /sys/class/graphics/fb0/window_axis
+            ;;
+            576*)
+            busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
+            busybox echo 30 20 689 555 > /sys/class/graphics/fb0/window_axis
+            ;;
+            720*)
+            busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
+            busybox echo 40 30 1239 689 > /sys/class/graphics/fb0/window_axis
+            ;;
+            1080*)
+            busybox echo 0 0 1919 1079 > /sys/class/graphics/fb0/free_scale_axis
+            busybox echo 60 40 1859 1039 > /sys/class/graphics/fb0/window_axis
+            ;;
+            4k2k*)
+            busybox echo 0 0 1919 1079 > /sys/class/graphics/fb0/free_scale_axis
+            busybox echo 120 80 3719 2079 > /sys/class/graphics/fb0/window_axis
+            ;;
+            *)
+            #Default to 720p
+            echo 720p > /sys/class/display/mode
+            busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
+            busybox echo 40 30 1239 689 > /sys/class/graphics/fb0/window_axis
+        esac
+
+        busybox echo 0x10001 > /sys/class/graphics/fb0/free_scale
+        busybox echo 1 > /sys/class/graphics/fb1/blank
     fi
-
-#>>>>>>> s82 #zgq remove
-	else
-		if [ "$outputmode" != "480cvbs" -a "$outputmode" != "576cvbs" ] ; then
-			outputmode=576cvbs
-		fi
-	fi
-	echo $outputmode > /sys/class/display/mode
-	busybox echo 0 > /sys/class/ppmgr/ppscaler
-	busybox echo 0 > /sys/class/graphics/fb0/free_scale
-	busybox echo 1 > /sys/class/graphics/fb0/freescale_mode
-
-	case $outputmode in 
-
-		480*)
-		busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
-		busybox echo 30 20 689 459 > /sys/class/graphics/fb0/window_axis 
-		;;
-    
-		576*)
-		busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
-		busybox echo 30 20 689 555 > /sys/class/graphics/fb0/window_axis 
-		;; 
-
-		720*)
-		busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis
-		busybox echo 40 30 1239 689 > /sys/class/graphics/fb0/window_axis 
-		;; 
-
-		1080*)
-		busybox echo 0 0 1919 1079 > /sys/class/graphics/fb0/free_scale_axis
-		busybox echo 60 40 1859 1039 > /sys/class/graphics/fb0/window_axis 
-		;; 
-
-		4k2k*)
-		busybox echo 0 0 1919 1079 > /sys/class/graphics/fb0/free_scale_axis
-		busybox echo 120 80 3719 2079 > /sys/class/graphics/fb0/window_axis 
-		;;
- 
-		*)
-		#outputmode= 720p
-		echo 720p > /sys/class/display/mode   
-		busybox echo 0 0 1279 719 > /sys/class/graphics/fb0/free_scale_axis 
-		busybox echo 40 30 1239 689 > /sys/class/graphics/fb0/window_axis 
-
-	esac
-busybox echo 0x10001 > /sys/class/graphics/fb0/free_scale
-busybox echo 1 > /sys/class/graphics/fb1/blank
-fi
-                                            
 done
-
-
-
-
-
